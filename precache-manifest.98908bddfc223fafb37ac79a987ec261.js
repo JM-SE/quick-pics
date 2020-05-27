a1a65cf77c3bfe12dd67e2cdc5a347c865c64468
Jose Maria Silva Ezpeleta <jsilva11@gmail.com>
2020-05-27 16:29:44 -0300

    Updates

---
+++ precache-manifest.98908bddfc223fafb37ac79a987ec261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9a6c0ab200994864e3ba26e4966d0c",
+    "url": "/quick-pics/index.html"
+  },
+  {
+    "revision": "28b0949362fd549bbf64",
+    "url": "/quick-pics/static/css/main.0b325abe.chunk.css"
+  },
+  {
+    "revision": "142725bf887f36ac5dcc",
+    "url": "/quick-pics/static/js/2.85ba8dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quick-pics/static/js/2.85ba8dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b0949362fd549bbf64",
+    "url": "/quick-pics/static/js/main.02cd013f.chunk.js"
+  },
+  {
+    "revision": "0e1287c1d8a955ada1d1",
+    "url": "/quick-pics/static/js/runtime-main.f1f953db.js"
+  }
+]);
